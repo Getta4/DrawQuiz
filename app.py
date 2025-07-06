19f8b9177aedc6fa596aaaa949560c1ede7c6f46
＝ <harharpuyo@gmail.com>
2025-07-06 21:46:58 +0900

    画像の判定機能を追加

--- app.py
+++ app.py
@@ -5,29 +5,21 @@ from google.cloud import vision
 import tkinter as tk
 import time
 
+from PIL import Image
+import io
+
 #変数の設定
 objects = []
 obj_score = []
 
-#キャンバスの設定
-window = tk.Tk()
-window.title("DrawQuiz!")
-cv = tk.Canvas(window,width=800,height=500)
-cv.pack()
-
 #正誤判定の動作(from Codelabs https://codelabs.developers.google.com/codelabs/cloud-vision-api-python?hl=ja#3 )
-def analyze_image_from_uri(
-    image_uri: str,
+def analyze_image_from_content(
+    image_content: bytes,
     feature_types: Sequence,
 ) -> vision.AnnotateImageResponse:
     client = vision.ImageAnnotatorClient()
 
-    image = vision.Image()
-
-    #ファイルの読み込み
-    with open(image_uri,'rb') as image_file:
-        content = image_file.read()
-    image.content = content
+    image = vision.Image(content=image_content)
 
     features = [vision.Feature(type_=feature_type) for feature_type in feature_types]
     request = vision.AnnotateImageRequest(image=image,features=features)
@@ -41,10 +33,40 @@ def get_labels(response: vision.AnnotateImageResponse):
     for label in response.label_annotations:
         objects.append(label.description.lower())
         obj_score.append(int(round(label.score,2)*100))
-'''
-image_uri = ""
-features = [vision.Feature.Type.LABEL_DETECTION]
-response = analyze_image_from_uri(image_uri, features)
-'''
+
+#描画関数
+def draw(event):
+    x1 = event.x - 3
+    y1 = event.y - 3
+    x2 = event.x + 3
+    y2 = event.y + 3
+    cv.create_oval(x1, y1, x2, y2, fill="black", outline="black")
+
+
+def save_canvas():
+#キャンバスの内容を取得
+    ps = cv.postscript(colormode='color')
+    img = Image.open(io.BytesIO(ps.encode('utf-8')))
+    
+    png_bytes_io = io.BytesIO()
+    img.save(png_bytes_io, format='png')
+    png_bytes = png_bytes_io.getvalue()
+
+    features = [vision.Feature.Type.LABEL_DETECTION]
+    response = analyze_image_from_content(png_bytes, features)
+    get_labels(response)
+    print(objects)
+    print(obj_score)
+
+#キャンバスの設定
+window = tk.Tk()
+window.title("DrawQuiz!")
+cv = tk.Canvas(window,width=800,height=500)
+cv.pack()
+
+button = tk.Button(window, text="Save and Analyze", command=save_canvas)
+button.pack()
+
+cv.bind("<B1-Motion>", draw)
 
 window.mainloop()
